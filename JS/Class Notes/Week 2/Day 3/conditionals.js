7de3807cc3c8a93065e7f44d7115c98b40d65562
kelebetengida <engida398@gmail.com>
2024-08-07 07:31:39 -0700

    Conditionals

---
+++ JS/Class Notes/Week 2/Day 3/conditionals.js
@@ -0,0 +1,54 @@
+/////////////////////////////////////Conditional statments///////////////
+
+// Consitional statments are statmets or decision based on different conditions.
+//By default js analyzes our code sequentialy so that there is flow in out page. 
+
+/////if/////////////////////////////////////////////////////////////
+
+//syntax
+// if(condition){
+//     //this is the part your truthy or falsy statment . 
+// }
+
+
+/////////////////if else ////////////////////////////////////////
+
+// if(condition){
+//     //this is the part your truthy or falsy statment . 
+// } else{ 
+//     //this is the part your truthy or falsy statment . 
+// }
+
+
+
+
+////////if esle if else /////////////////////////////////////////
+
+// if(condition){
+//     //code to returen first truthy 
+// } else if(condition){
+//     //code to returen a secound truthy value 
+// }else {
+//     // code to returen falsy value 
+// }
+
+
+
+
+/////////////////////////////////////////////////////Switch////////////////////////////////////////////////////////////
+
+// switch(caseValue){
+//     case 1:
+//         //code
+//         break
+//     case 2:
+//         //code
+//         break
+//     case 3:
+//         //code
+//         break
+//     case 4:
+//         //code
+//         break
+// }
+
