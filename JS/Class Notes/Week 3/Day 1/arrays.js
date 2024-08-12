1a68c4c98411700c957677e6d637efe9ad5ede70
kelebetengida <engida398@gmail.com>
2024-08-12 06:01:59 -0700

    Array

---
+++ JS/Class Notes/Week 3/Day 1/arrays.js
@@ -0,0 +1,84 @@
+///////////////////////////////////////////////Welcome To Arrays///////////////////////////////
+///////////////////////////////////////////Non-primitive data type/////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+// How to create an empty array. 
+// This the most recommended way to create an empty list. Here is a general syntax
+// let arr=[]
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// How to create an array with values
+
+
+//.........................................To Do..............................................
+//Create an array with values and then console log the list and its length  
+// array of numbers
+
+
+
+// array of strings, fruits
+
+
+// array of web technologies
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////
+// Creating an array using split
+
+
+//.........................................To Do..............................................
+//For the two strings use a split method to conosole.log and see the list of arrays
+
+
+
+
+
+// /////////////////////////////////////////////////////////////////////////////////////////////
+//Accessing array items using index
+
+const fruits = ['banana', 'orange', 'mango', 'lemon']
+//index of Fruit////0/////////1////////2////////3////
+
+
+
+
+
+//.........................................To Do..............................................
+//console how many are in  my shoping bag, just accsess and pring potato, Milk and sugar. Replace Avocado with Onion
+const shoppingCart = [
+    'Milk',
+    'Mango',
+    'Tomato',
+    'Potato',
+    'Avocado',
+    'Meat',
+    'Eggs',
+    'Sugar'
+  ] // List of food products
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Modifying array element
+const numbers = [1, 2, 3, 4, 5]
+/////////////////0//1//2
+
+
+
+
+//////////////////////////////////////////////////////////////////////////////////////////////
+// Methods to manipulate array
+
+
+
+
+
+//Getting index an element in arr array (To check if an item exist in an array.)
+
+
+
+
+//Getting index an element in arr array (To check if an item exist in an array.)
