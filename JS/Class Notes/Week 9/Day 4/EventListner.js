14dc6e5ddc6408a0b9f046f2fab0440c9edbc70f
kelebetengida <engida398@gmail.com>
2024-10-03 07:08:07 -0700

    Event

---
+++ JS/Class Notes/Week 9/Day 4/EventListner.js
@@ -0,0 +1,31 @@
+// Event Listeners
+
+    // Click
+
+
+
+    // Double Click
+
+
+
+    // Mouse enter
+
+
+
+// Getting value from an input element
+
+
+
+// input value
+
+
+
+    // input event and change
+
+
+
+    // blur event
+
+
+    
+    // keypress, keydow and keyup
