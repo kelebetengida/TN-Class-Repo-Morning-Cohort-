bfd4b802f7e3dc7aac449f2647c32cd2fe8aeecb
kelebetengida <engida398@gmail.com>
2024-10-21 08:49:58 -0700

    JSX starter

--- React/Class  Notes/jsx/src/index.js
+++ React/Class  Notes/jsx/src/index.js
@@ -1,17 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//This is to import react library for the page to start runing JSX and react code
+import React from "react"
+//ReactDOM allows us to render the react code, JSX, Component to the root div in HTML
+import ReactDOM from "react-dom"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+//JSX- WHen we are trying to render a simple html jsx element Just store it in a variable. 
+const title=<h1>Welcome to React</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+
+
+
+
+
+//This root variable is refering to the root elemet in the index flie
+const root = document.getElementById("root")
+//WHere ReactDOM uses the render method which takes the code that needs to be rendered and where it will be rendered
+ReactDOM.render(title, root )
